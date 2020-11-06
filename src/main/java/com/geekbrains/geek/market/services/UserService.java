87e7e104df76b1c62ba34c2ed7abe07c79d52ffa
undel <deld67@gmail.com>
2020-11-06 10:29:01 +0300

    hw  lesson 2 final

--- src/main/java/com/geekbrains/geek/market/services/UserService.java
+++ src/main/java/com/geekbrains/geek/market/services/UserService.java
@@ -3,6 +3,7 @@ package com.geekbrains.geek.market.services;
 
 import com.geekbrains.geek.market.entities.Role;
 import com.geekbrains.geek.market.entities.User;
+import com.geekbrains.geek.market.exceptions.ResourceNotFoundException;
 import com.geekbrains.geek.market.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,17 +25,15 @@ import java.util.stream.Collectors;
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
-    public User findByUsername(String username) {
+    public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = findByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException(String.format("User '%s' not found", username));
-        }
+        User user = findByUsername(username).orElseThrow(()->new UsernameNotFoundException(String.format("User '%s' not found", username) ));
+
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
     }
 
